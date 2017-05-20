164c31eedca0ffdd7ca44960c16b0dea6e52ebe5
Shailendra Gupta <guptas199614@gmail.com>
2017-05-21 00:20:05 +0530

    Initial Commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var clean = require('gulp-clean');
+var gulp     = require('gulp');
+var sass     = require('gulp-sass');
+var useref   = require('gulp-useref');
+var uglify   = require('gulp-uglify');
+var gulpIf   = require('gulp-if');
+var clean    = require('gulp-clean');
+var imagemin = require('gulp-imagemin');
+var ngHTML   = require('gulp-angular-htmlify');
+var plumber  = require('gulp-plumber');
+var srcMap   = require('gulp-sourcemaps');
 
 gulp.task('default', function () {
 
-});
-
-gulp.task('hello', function () {
-    console.log('Hello Zell');
 });
 
 gulp.task('sass', function () {
@@ -19,7 +19,7 @@ gulp.task('sass', function () {
             .pipe(gulp.dest('src/css/'));
 });
 
-gulp.task('user_ref', function () {
+gulp.task('userref', function () {
     return gulp.src('src/index.html')
             .pipe(useref())
             .pipe(gulpIf('*.js', uglify()))
@@ -33,4 +33,14 @@ gulp.task('watch', function () {
 gulp.task('clean', function () {
     return gulp.src(['dist/css/*.css','dist/js/*.js','dist/fonts/*','dist/templates/*.html','dist/images/*', 'dist/*.html'], {read: false})
             .pipe(clean({force: true}));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src('src/fonts/**/*')
+            .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('templates', function () {
+    return gulp.src('src/templates/**/*')
+            .pipe(gulp.dest('dist/templates'));
 });
